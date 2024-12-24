0130dad9acae6228f8fa3f528a52befde5134333
Khushi Ka.Patel <24cs032@charusat.edu.in>
2024-12-24 20:55:42 +0530

    project completed.

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+//Project made by KHUSHI KAPATEL
+//Students Management System Using C Language.
 #include<stdio.h>
 #include<string.h>
 
@@ -16,12 +18,16 @@ int Add_Student_details(struct Student_details *ptr , int index);
 void Sorting_StuDetails(struct Student_details *ptr , int total_stu);
 void PrintingDetails_inFile(struct Student_details *ptr , int total_stu);
 void Edit_stuDetails(struct Student_details *ptr , int total_stu);
-
+void ViewAll_StuDetails(struct Student_details *ptr, int total_stu);
+int Delete_detailOfstu(struct Student_details *ptr , int index);
+void Search_stuDetails(struct Student_details *ptr,int total_stu);
+void PrintingFinalDetails_inFile(struct Student_details *ptr ,int total_stu);
 
 
 void main()
 {
     int choice,lastStu=0;
+    printf("\n");
     printf("----------Welcome To CHARUSAT----------\n");
     printf("\n");
     printf("---------------------------------------\n");
@@ -34,6 +40,7 @@ void main()
     printf("4. View All Student's Details.\n");
     printf("5. Delete Student's Details.\n");
     printf("6. Search for any Student.\n");
+    printf("7. For Printing Final Details.\n");
     printf("\n Enter 0 for Exit.\n");
     printf("---------------------------------------\n");
     restart:
@@ -48,8 +55,16 @@ void main()
                  goto restart;
         case 3 : Edit_stuDetails(&stu[0],lastStu);
                  goto restart;
-        case 4 : 
-        case 0 : printf("THANK YOU!!");
+        case 4 : ViewAll_StuDetails(&stu[0],lastStu);
+                 goto restart;
+        case 5 : lastStu = Delete_detailOfstu(&stu[0],lastStu);
+                 goto restart;
+        case 6 : Search_stuDetails(&stu[0],lastStu);
+                 goto restart;
+        case 7 : PrintingFinalDetails_inFile(&stu[0],lastStu);
+        case 0 : printf("----------------------------------------\n");
+                 printf("THANK YOU!!\n");
+                 printf("----------------------------------------\n");
                  break ;
                 
 
@@ -78,19 +93,19 @@ void Sorting_StuDetails(struct Student_details *ptr , int total_stu)
 void PrintingDetails_inFile(struct Student_details *ptr , int total_stu)
 {
     FILE *fptr;
-        fptr = fopen("student_record.txt","w");
-        for(int i=0 ; i<total_stu ; i++)
-        {
-            fputs((ptr+i)->stuID , fptr );
-            fprintf(fptr ,"\t\t\t");
-            fprintf(fptr , "%s" , (ptr+i)->stuName);
-            fprintf(fptr ,"\t\t");
-            fprintf(fptr,"%f\t\t",(ptr+i)->math_marks);
-            fprintf(fptr,"%f\t\t",(ptr+i)->ccp_marks);
-            fprintf(fptr,"%f\t\t",(ptr+i)->beee_marks);
-            fprintf(fptr,"%f\n",(ptr+i)->total);
-        }
-        fclose(fptr);
+    fptr = fopen("student_record.txt","w");
+    for(int i=0 ; i<total_stu ; i++)
+    {
+        fputs((ptr+i)->stuID , fptr );
+        fprintf(fptr ,"\t\t\t");
+        fprintf(fptr , "%s" , (ptr+i)->stuName);
+        fprintf(fptr ,"\t\t");
+        fprintf(fptr,"%f\t\t",(ptr+i)->math_marks);
+        fprintf(fptr,"%f\t\t",(ptr+i)->ccp_marks);
+        fprintf(fptr,"%f\t\t",(ptr+i)->beee_marks);
+        fprintf(fptr,"%f\n",(ptr+i)->total);
+    }
+    fclose(fptr);
 }
 
 
@@ -98,15 +113,16 @@ void PrintingDetails_inFile(struct Student_details *ptr , int total_stu)
 int Students_details(struct Student_details *ptr)
 {
     int numOfStu;
-    printf("----------------------------------------\n\n");
-    printf("Now You can add Details of Students.\n");
+    printf("----------------------------------------\n");
+    printf("Now You can add Details of Students.\n\n");
     printf("Enter Number Of Student:");
     scanf("%d",&numOfStu);
     getchar();
 
     for(int i=0 ; i<numOfStu ;i++)
     {
-        printf("Enter Details of Student no. %d\n",i+1);
+        printf("----------------------------------------\n");
+        printf("Enter Details of Student no. %d\n\n",i+1);
         
         printf("Enter Name of Student:");
         fgets((ptr+i)->stuName,sizeof((ptr+i)->stuName),stdin);
@@ -127,7 +143,7 @@ int Students_details(struct Student_details *ptr)
         
         getchar();
      
-        printf("Enter maths's mark:");
+        printf("Enter Maths's mark:");
         scanf("%f",&(ptr+i)->math_marks);
 
         printf("Enter CCP's mark:");
@@ -164,6 +180,7 @@ int Add_Student_details(struct Student_details *ptr , int index)
     }
     printf("----------------------------------------\n\n");
     printf("Now You can add Details of Students in existing list.\n\n");
+    printf("----------------------------------------\n");
     printf("Enter Name of Student:");
     fgets((ptr+index)->stuName,sizeof((ptr+index)->stuName),stdin);
     size_t len = strlen((ptr + index)->stuName);
@@ -182,7 +199,7 @@ int Add_Student_details(struct Student_details *ptr , int index)
         
     getchar();
      
-    printf("Enter maths's mark:");
+    printf("Enter Maths's mark:");
     scanf("%f",&(ptr+index)->math_marks);
 
     printf("Enter CCP's mark:");
@@ -212,24 +229,21 @@ void Edit_stuDetails(struct Student_details *ptr , int total_stu)
     if(total_stu==0)
     {
         printf("----------------------------------------\n");
-        printf("There is no existing list of Student Details>\n");
+        printf("There is no existing list of Student Details.\n");
         printf("\nTHANK YOU!!\n");
         printf("--------------------------------------\n");
    
     }
     else
     {
+        int flag=1;
         printf("----------------------------------------\n");
         printf("Now You Can Edit any Student's Detail via\n");
         printf("              Student ID\n");
         char id[8];
         printf("Enter Student ID:");
         scanf("%s",id);
-        size_t len = strlen(id);
-        if (id[len - 1] == '\n')
-        {
-            id[len - 1] = '\0';
-        }
+        
         getchar();
         for(int i=0 ; i<total_stu ; i++)
         {
@@ -270,16 +284,152 @@ void Edit_stuDetails(struct Student_details *ptr , int total_stu)
                 Sorting_StuDetails(ptr,total_stu);
                 PrintingDetails_inFile(ptr,total_stu);
                 printf("Edit is Successfuly Done.!!\n");
+                flag=0;
             }
-            else
-            {
-                printf("There is no such ID.\n");
-        
-            }
+            
+        }
+        if(flag)
+        {
+            printf("There is no such ID.\n");
         }
         printf("THANK YOU!!\n");
         printf("--------------------------------------\n");
      
     }
 
+}
+
+void ViewAll_StuDetails(struct Student_details *ptr, int total_stu)
+{
+    printf("--------------------------------------\n\n");
+    for(int i=0 ; i<total_stu ; i++)
+    {
+        printf("Name : ");
+        puts((ptr+i)->stuName);
+        printf("\n");
+        printf("ID: ");
+        puts((ptr+i)->stuID);
+        printf("\n");
+        printf("Marks of Maths: %f\n",(ptr+i)->math_marks);
+        printf("Marks of CCP  : %f\n",(ptr+i)->ccp_marks);
+        printf("Marks of BEEE : %f\n",(ptr+i)->beee_marks);
+        printf("Total Marks   : %f\n",(ptr+i)->total);
+        printf("--------------------------------------\n");
+    }
+    printf("THANK YOU!!\n");
+    printf("--------------------------------------\n");
+
+}
+
+int Delete_detailOfstu(struct Student_details *ptr , int index)
+{
+    int flag = 1;
+    getchar();
+    if(index==0)
+    {
+        printf("--------------------------------------\n");
+        printf("There is no existing list of Student Details.\n");
+        printf("THANK YOU!!\n");
+        printf("--------------------------------------\n");
+        return 0;
+    }
+    char delID[8];
+    printf("--------------------------------------\n");
+    printf("Enter that ID , which's details Do you want to delete!!\n\n");
+    printf("Enter ID :");
+    scanf("%s",delID);
+    for(int i=0 ; i<index ; i++)
+    {
+        if(strcmp(delID,(ptr+i)->stuID)==0)
+        {
+            for(int j=i ; j<index ; j++)
+            {
+                ptr[j]=ptr[j+1];
+            }
+            flag=0;
+        }
+        
+        
+    }
+    if(flag)
+    {
+        printf("No Such ID Found!!");
+        printf("THANK YOU!!\n");
+        printf("--------------------------------------\n");
+        return 0;
+    }
+    index=index-1;
+    PrintingDetails_inFile(ptr,index);
+    printf("\nDetails Deleted Successfuly!!\n");
+    printf("THANK YOU!!\n");
+    printf("--------------------------------------\n");
+    return index;
+
+}
+
+
+void Search_stuDetails(struct Student_details *ptr,int total_stu)
+{
+    int flag=1;
+    getchar();
+    if(total_stu==0)
+    {
+        printf("----------------------------------------\n");
+        printf("There is no existing list of Student Details>\n");
+        printf("\nTHANK YOU!!\n");
+        printf("--------------------------------------\n");
+
+    }
+    else
+    {
+        char id[8];
+        printf("----------------------------------------\n");
+        printf("Now You Can Search Any Student's Details\n");
+        printf("               Via Student ID\n\n");
+        printf("Enter ID:");
+        scanf("%s",id);
+        for(int i=0 ; i<total_stu ; i++)
+        {
+            if(strcmp(id,(ptr+i)->stuID)==0)
+            {
+                printf("Name : ");
+                puts((ptr+i)->stuName);
+                printf("\n");
+                printf("ID: ");
+                puts((ptr+i)->stuID);
+                printf("\n");
+                printf("Marks of Maths: %f\n",(ptr+i)->math_marks);
+                printf("Marks of CCP  : %f\n",(ptr+i)->ccp_marks);
+                printf("Marks of BEEE : %f\n",(ptr+i)->beee_marks);
+                printf("Total Marks   : %f\n",(ptr+i)->total);
+                printf("--------------------------------------\n");
+                flag=0;
+            }
+            
+        }
+        if (flag)
+        {
+            printf("\nThere is no such ID.\n");
+        }
+        printf("\nTHANK YOU!!\n");
+        printf("--------------------------------------\n");
+    }
+}
+
+void PrintingFinalDetails_inFile(struct Student_details *ptr ,int total_stu)
+{
+    FILE *fptr;
+    fptr = fopen("Final_stu_record.txt","a");
+    for(int i=0 ; i<total_stu ; i++)
+    {
+        fputs((ptr+i)->stuID , fptr );
+        fprintf(fptr ,"\t\t\t");
+        fprintf(fptr , "%s" , (ptr+i)->stuName);
+        fprintf(fptr ,"\t\t");
+        fprintf(fptr,"%f\t\t",(ptr+i)->math_marks);
+        fprintf(fptr,"%f\t\t",(ptr+i)->ccp_marks);
+        fprintf(fptr,"%f\t\t",(ptr+i)->beee_marks);
+        fprintf(fptr,"%f\n",(ptr+i)->total);
+    }
+    fclose(fptr);
 }
